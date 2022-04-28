eb33b855769fc82f4fa93a2a83c7db38f3c6c968
Harsh sharma <40464347+HarshXharsH@users.noreply.github.com>
2022-04-28 20:42:48 +0530

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,9 +9,9 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'DPSRU Alumni Portal',
+  domain: 'dpsru.vercel.app',
+  author: 'Harsh Sharma',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
