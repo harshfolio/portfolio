e0683e8d405eaf82b460bec2eab55bdd81d3bdb4
Harsh sharma <40464347+harshfolio@users.noreply.github.com>
2024-02-21 22:33:15 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import * as analytics from 'vercel/analytics';
+import * as analytics from 'vercel/analytics'
 
 import { NotionPage } from '@/components/NotionPage'
 import { domain } from '@/lib/config'
